04dc6536e900034a5b78972d2e40c5b33f7ce969
Alvaro Resende <alvinhoresende2000@hotmail.com>
2025-09-08 01:49:53 -0300

    Fifth Commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,16 @@ import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
       "@": path.resolve("./"), // Alias @ → raiz do projeto
     };
     return config;
+  },
+  experimental: {
+    appDir: true, // Se você estiver usando /app
   },
 };
 
